20dd3a7fb09ca00378ea5e22dc8d2877245e3c7b
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2018-12-23 23:13:33 +0100

    Cleanup

--- Scripts/Builders/DispatchBuilder.rb
+++ Scripts/Builders/DispatchBuilder.rb
@@ -13,26 +13,17 @@ class DispatchBuilder < Builder
       execute "mkdir -p #{@builds}"
    end
 
-   def args
-      swift = SwiftBuilder.new(@arch)
-      cmd = []
-      cmd << "CLANG=\"#{swift.builds}/llvm-linux-x86_64/bin/clang\""
-      cmd << "CC=\"#{swift.builds}/llvm-linux-x86_64/bin/clang\""
-      cmd << "CXX=\"#{swift.builds}/llvm-linux-x86_64/bin/clang++\""
-      cmd << "SWIFT=\"#{swift.builds}/swift-linux-x86_64/bin/swift\""
-      cmd << "SWIFTC=\"#{swift.builds}/swift-linux-x86_64/bin/swiftc\""
-      return cmd
-   end
-
-   def options
+   def configure
       swift = SwiftBuilder.new(@arch)
       ndk = AndroidBuilder.new(@arch)
       # See: /swift/swift-corelibs-libdispatch/INSTALL.md
-      cmd = []
-      # Seems this cause binary to be x86_64 instead of arm.
-      # cmd << "-DCMAKE_C_COMPILER=#{swift.builds}/llvm-linux-x86_64/bin/clang"
-      # cmd << "-DCMAKE_CXX_COMPILER=#{swift.builds}/llvm-linux-x86_64/bin/clang++"
+      prepare
+      configurePatches(false)
+      configurePatches
 
+      cmd = []
+      cmd << "cd #{@builds} &&"
+      cmd << "cmake -G Ninja"
       cmd << "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a"
       cmd << "-DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang"
       cmd << "-DCMAKE_ANDROID_STL_TYPE=\"c++_static\""
@@ -41,25 +32,11 @@ class DispatchBuilder < Builder
       cmd << "-DENABLE_TESTING=false"
       cmd << "-DCMAKE_SWIFT_COMPILER=\"#{swift.builds}/swift-linux-x86_64/bin/swiftc\""
       cmd << "-DCMAKE_PREFIX_PATH=\"#{swift.builds}/swift-linux-x86_64/lib/cmake/swift\""
-      cmd << "-DCMAKE_INSTALL_PREFIX=#{@installs}"
+      cmd << "-DCMAKE_INSTALL_PREFIX=#{swift.installs}/usr" # Applying Dispatch over existing file structure.
       cmd << "-DCMAKE_SYSTEM_NAME=Android"
       cmd << "-DCMAKE_SYSTEM_VERSION=#{ndk.api} -DCMAKE_ANDROID_NDK=#{ndk.sources}"
-
-      return cmd
-   end
-
-   def configure
-      prepare
-      configurePatches(false)
-      configurePatches
-      cmd = []
-      cmd << "cd #{@builds} &&"
-      # cmd += args
-      cmd << "cmake -G Ninja"
-      cmd += options
       cmd << @sources
       execute cmd.join(" ")
-      # configurePatches(false)
       fixNinjaBuild
       logConfigureCompleted
    end
@@ -87,50 +64,24 @@ class DispatchBuilder < Builder
    end
 
    def build
-      # ndk = AndroidBuilder.new(@arch)
-      # sysroot = ndk.installs + "/sysroot"
-
-      # setupLinkerSymLink()
-      cmd = ["cd #{@builds} &&"]
-      # cmd << "SWIFTCFLAGS='-tools-directory #{ndk.bin}'"
-      # cmd << "CFLAGS='-isystem #{sysroot}/usr/include'"
-      # cmd << "LDFLAGS=\"--sysroot=#{sysroot} -L#{ndk.sources}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.9.x -L#{sysroot}/usr/lib\""
-      cmd << "ninja"
-      execute cmd.join(" ")
-      # configurePatches(false)
-      # setupLinkerSymLink(false)
-      return
-
-      prepare
-      configurePatches(false)
-      configurePatches
-      ndk = AndroidBuilder.new(@arch)
-      # FIXME: Seems we need to simlink GOLD Linker like in Swift. See CMakeLists.txt file
-      # See: What is CMake equivalent of 'configure --prefix=DIR && make all install: https://stackoverflow.com/a/35753015/1418981
-      cmd = ["cd #{@builds} &&"]
-      cmd << "PATH=#{ndk.installs}/bin:$PATH"
-      cmd << "cmake"
-      cmd += options
-      cmd << " . && "
-      # cmd += args
-      cmd << "ninja install"
-      execute cmd.join(" ")
-      configurePatches(false)
+      execute "cd #{@builds} && ninja"
       logBuildCompleted
    end
 
    def install
       execute "cd #{@builds} && ninja install"
+      logInstallCompleted
    end
 
    def make
       configure
       build
+      install
+      configurePatches(false)
    end
 
    def clean
       execute "rm -rf \"#{@builds}\""
-      execute "rm -rf \"#{@installs}\""
    end
 
    def checkout
