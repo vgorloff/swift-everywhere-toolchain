c2f0d81456aa04574fa712591c9d01ea26be9899
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2018-12-27 22:46:55 +0100

    attemot to build swift.

--- Scripts/Builders/SwiftBuilder.rb
+++ Scripts/Builders/SwiftBuilder.rb
@@ -64,11 +64,11 @@ class SwiftBuilder < Builder
       cmd << "cd #{@builds} &&"
       cmd << "cmake -G Ninja"
 
-      # cmd << "-DCMAKE_C_COMPILER=\"#{@llvm.bin}/clang\""
-      # cmd << "-DCMAKE_CXX_COMPILER=\"#{@llvm.bin}/clang++\""
+      cmd << "-DCMAKE_C_COMPILER=\"#{llvmBuilder.builds}/bin/clang\""
+      cmd << "-DCMAKE_CXX_COMPILER=\"#{llvmBuilder.builds}/bin/clang++\""
 
-      cmd << "-DCMAKE_C_COMPILER=/usr/bin/clang"
-      cmd << "-DCMAKE_CXX_COMPILER=/usr/bin/clang++"
+      # cmd << "-DCMAKE_C_COMPILER=/usr/bin/clang"
+      # cmd << "-DCMAKE_CXX_COMPILER=/usr/bin/clang++"
 
       # See:
       # - https://stackoverflow.com/questions/10712972/what-is-the-use-of-fno-stack-protector
@@ -83,6 +83,7 @@ class SwiftBuilder < Builder
       # cmd << "-DCMAKE_CXX_FLAGS="-Wno-c++98-compat -Wno-c++98-compat-pedantic"^
       # cmd << "-DCMAKE_EXE_LINKER_FLAGS:STRING="/INCREMENTAL:NO"^
       # cmd << "-DCMAKE_SHARED_LINKER_FLAGS="/INCREMENTAL:NO"^
+
       cmd << "-DCMAKE_INSTALL_PREFIX=\"#{@installs}\""
       cmd << "-DCMAKE_BUILD_TYPE=Release"
       cmd << "-DSWIFT_INCLUDE_TESTS=NO"
@@ -90,18 +91,33 @@ class SwiftBuilder < Builder
       cmd << "-DSWIFT_BUILD_SOURCEKIT=NO"
       cmd << "-DSWIFT_BUILD_SYNTAXPARSERLIB=NO"
 
+      # cmd << "-DLLVM_TOOLS_BINARY_DIR=#{llvmBuilder.bin}"
+      # cmd << "-DLLVM_BUILD_LIBRARY_DIR=#{llvmBuilder.lib}"
+      # cmd << "-DLLVM_BUILD_MAIN_INCLUDE_DIR=#{llvmBuilder.include}"
+      # cmd << "-DLLVM_BUILD_BINARY_DIR=\"#{llvmBuilder.bin}\""
+      # cmd << "-DLLVM_BUILD_MAIN_SRC_DIR=\"#{llvmBuilder.sources}\""
+      # cmd << "-DLLVM_LIBRARY_DIRS=#{llvmBuilder.lib}"
+
+      # cmd << "-DCMAKE_MODULE_PATH=#{llvmBuilder.lib}/cmake/llvm"
+      # cmd << "-DClang_DIR=#{llvmBuilder.lib}/cmake/clang"
+
+
+      # cmd << "-DCLANG_MAIN_INCLUDE_DIR=\"#{llvmBuilder.include}\""
+
+      # cmd << "-DLLVM_PACKAGE_VERSION=7.0.0"
+
       # Making Ninja verbose. See: https://github.com/ninja-build/ninja/issues/900#issuecomment-132346047
       # cmd << "-DCMAKE_VERBOSE_MAKEFILE=ON"
 
-      # cmd << "-DSWIFT_PATH_TO_LLVM_SOURCE=\"#{llvmBuilder.sources}\""
-      # cmd << "-DSWIFT_PATH_TO_LLVM_BUILD=\"#{llvm}\""
+      cmd << "-DSWIFT_PATH_TO_LLVM_SOURCE=\"#{llvmBuilder.sources}\""
+      cmd << "-DSWIFT_PATH_TO_LLVM_BUILD=\"#{llvmBuilder.builds}\""
 
-      # cmd << "-DSWIFT_PATH_TO_CMARK_SOURCE=\"#{cmarkBuilder.sources}\""
-      # cmd << "-DSWIFT_PATH_TO_CMARK_BUILD=\"#{cmark}\""
+      cmd << "-DSWIFT_PATH_TO_CMARK_SOURCE=\"#{cmarkBuilder.sources}\""
+      cmd << "-DSWIFT_PATH_TO_CMARK_BUILD=\"#{cmarkBuilder.builds}\""
       # cmd << "-DSWIFT_CMARK_LIBRARY_DIR=\"#{@cmark.lib}\""
 
-      cmd << "-DSWIFT_SDKS=LINUX"
-      cmd << "-DLLVM_DIR=#{llvmBuilder.sources}/cmake/modules"
+      # cmd << "-DSWIFT_SDKS=LINUX"
+      # cmd << "-DLLVM_DIR=#{llvmBuilder.sources}/cmake/modules"
 
       # Seems these vars is unused.
       # cmd << "-DSWIFT_EXEC=#{@builds}/bin/swiftc"
@@ -110,14 +126,8 @@ class SwiftBuilder < Builder
       # cmd << "-DSWIFT_PATH_TO_CLANG_SOURCE=\"#{@clang.sources}\""
       # cmd << "-DSWIFT_PATH_TO_LIBDISPATCH_SOURCE=\"#{dispatchBuilder.sources}\""
       # cmd << "-DSWIFT_PATH_TO_CLANG_BUILD=\"#{@llvm.builds}\""
-      # cmd << "-DLLVM_BUILD_LIBRARY_DIR=\"#{@llvm.lib}\""
-      # cmd << "-DLLVM_BUILD_MAIN_INCLUDE_DIR=\"#{@llvm.include}\""
-      # cmd << "-DLLVM_BUILD_BINARY_DIR=\"#{@llvm.bin}\""
-      # cmd << "-DLLVM_BUILD_MAIN_SRC_DIR=\"#{@llvm.sources}\""
 
       # Both lines not needed after setting C/CXX compillers
-      # cmd << "-DCMAKE_PREFIX_PATH=#{@llvm.lib}/cmake/clang"
-      # cmd << "-DClang_DIR=#{@llvm.lib}/cmake/clang"
       #
       # -DSWIFT_WINDOWS_x86_64_ICU_UC_INCLUDE="%swift_source_dir%/icu/include"^
       # -DSWIFT_WINDOWS_x86_64_ICU_UC="%swift_source_dir%/icu/lib64/icuuc.lib"^
@@ -126,6 +136,12 @@ class SwiftBuilder < Builder
       cmd << @sources
       execute cmd.join(" ")
       logConfigureCompleted
+   end
+
+   def build_
+      logBuildStarted
+      execute "cd #{@builds} && ninja"
+      logBuildCompleted()
    end
 
    def build
