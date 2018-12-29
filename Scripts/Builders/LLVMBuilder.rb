d87d4dafe3e18c51fd3bc99c01c947b9442a3dc5
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2018-12-29 20:23:34 +0100

    MacOS compatibility update.

--- Scripts/Builders/LLVMBuilder.rb
+++ Scripts/Builders/LLVMBuilder.rb
@@ -24,9 +24,17 @@ class LLVMBuilder < Builder
       cmd << "-DLLVM_INCLUDE_DOCS=false"
       cmd << "-DLLVM_TARGETS_TO_BUILD=\"ARM;AArch64;X86\""
 
-      cmd << "-DCMAKE_C_COMPILER:PATH=/usr/bin/clang"
-      cmd << "-DCMAKE_CXX_COMPILER:PATH=/usr/bin/clang++"
-      cmd << "-DCMAKE_LIBTOOL:PATH="
+      cmd << "-DCMAKE_C_COMPILER:PATH=#{clang}"
+      cmd << "-DCMAKE_CXX_COMPILER:PATH=#{clang}++"
+      # cmd << "-DCMAKE_LIBTOOL:PATH="
+      if isMacOS?
+         cmd << "-DCMAKE_LIBTOOL=#{toolchainPath}/usr/bin/libtool"
+         cmd << "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.12"
+         cmd << "-DCMAKE_OSX_SYSROOT=#{macOSSDK}"
+         cmd << "-DSANITIZER_MIN_OSX_VERSION=10.12"
+         cmd << "-DLLVM_ENABLE_MODULES:BOOL=FALSE"
+         cmd << "-DLLVM_HOST_TRIPLE:STRING=x86_64-apple-macosx10.12"
+      end
       cmd << "-DLLVM_VERSION_MAJOR:STRING=7"
       cmd << "-DLLVM_VERSION_MINOR:STRING=0"
       cmd << "-DLLVM_VERSION_PATCH:STRING=0"
@@ -34,15 +42,16 @@ class LLVMBuilder < Builder
       cmd << "-DCLANG_VERSION_MINOR:STRING=0"
       cmd << "-DCLANG_VERSION_PATCH:STRING=0"
       cmd << "-DLLVM_ENABLE_ASSERTIONS=TRUE"
-      cmd << "-DCMAKE_C_FLAGS=' -Wno-unknown-warning-option -Werror=unguarded-availability-new -fno-stack-protector'"
-      cmd << "-DCMAKE_CXX_FLAGS=' -Wno-unknown-warning-option -Werror=unguarded-availability-new -fno-stack-protector'"
-      cmd << "-DLLVM_TOOL_SWIFT_BUILD:BOOL=NO"
-      cmd << "-DLLVM_ENABLE_LTO:STRING="
-      cmd << "-DLLVM_TOOL_COMPILER_RT_BUILD:BOOL=TRUE"
-      cmd << "-DLLVM_BUILD_EXTERNAL_COMPILER_RT:BOOL=TRUE"
+      cFlags = "-Wno-unknown-warning-option -Werror=unguarded-availability-new -fno-stack-protector"
+      cmd << "-DCMAKE_C_FLAGS='#{cFlags}'"
+      cmd << "-DCMAKE_CXX_FLAGS='#{cFlags}'"
+      cmd << "-DLLVM_TOOL_SWIFT_BUILD=NO"
+      # cmd << "-DLLVM_ENABLE_LTO:STRING="
+      cmd << "-DLLVM_TOOL_COMPILER_RT_BUILD=TRUE"
+      cmd << "-DLLVM_BUILD_EXTERNAL_COMPILER_RT=TRUE"
       cmd << "-DLLVM_LIT_ARGS=-sv"
-      cmd << "-DCMAKE_INSTALL_PREFIX=#{@installs}"
-      # cmd << "-DCMAKE_INSTALL_PREFIX:PATH=/usr/"
+      # cmd << "-DCMAKE_INSTALL_PREFIX=#{@installs}"
+      cmd << "-DCMAKE_INSTALL_PREFIX=/usr/"
       cmd << "-DINTERNAL_INSTALL_PREFIX=local"
 
       cmd << @sources
@@ -86,7 +95,11 @@ class LLVMBuilder < Builder
       crt = CompilerRTBuilder.new()
       setupSymLink(clang.sources, "#{@sources}/tools/clang")
       setupSymLink(crt.sources, "#{@sources}/projects/compiler-rt")
-      setupSymLink("/usr/include/c++", "#{@builds}/include/c++")
+      if isMacOS?
+         setupSymLink("#{toolchainPath}/usr/include/c++", "#{@builds}/include/c++")
+      else
+         setupSymLink("/usr/include/c++", "#{@builds}/include/c++")
+      end
    end
 
    def clean
