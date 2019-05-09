1dd1bf5857cdf76d8cd7969422e3a8c8415eee8a
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2019-05-09 02:00:52 +0200

    Removed old way of building Projects.

--- Projects/HelloExeBuilder.rb
+++ Projects/HelloExeBuilder.rb
@@ -10,59 +10,9 @@ class HelloExeBuilder < ProjectBuilder
    end
 
    def executeBuild
-      executeBuildNew
-   end
-
-   def executeBuildNew
-      execute "cd #{@builds} && #{@swiftc} -emit-executable -o #{@binary} #{@sources}/main.swift"
-      execute "file #{@binary}"
-      copyLibs()
-   end
-
-   def executeBuildOld
-      mainFile = "#{@builds}/main.o"
-
-      # Swift
-      cmd = ["cd #{@builds} &&"]
-      cmd << "#{@swift.installs}/bin/swift -frontend -c"
-      cmd << "-primary-file #{@sources}/hello.swift"
-      cmd << "-target armv7-none-linux-android -disable-objc-interop"
-      cmd << "-color-diagnostics -module-name hello -o #{mainFile}"
-      cmd << "-Xcc -I#{@ndk.toolchain}/sysroot/usr/include -Xcc -I#{@ndk.toolchain}/sysroot/usr/include/arm-linux-androideabi"
-      cmd << "-Xcc -DDEPLOYMENT_TARGET_ANDROID -Xcc -DDEPLOYMENT_TARGET_LINUX -Xcc -DDEPLOYMENT_RUNTIME_SWIFT"
-      cmd << "-I #{@dispatch.installs}/lib/swift/dispatch"
-      cmd << "-I #{@dispatch.installs}/lib/swift/android/armv7"
-      cmd << "-I #{@dispatch.installs}/lib/swift"
-      cmd << "-I #{@foundation.installs}/lib/swift/android/armv7"
-      cmd << "-I #{@foundation.installs}/lib/swift/CoreFoundation"
-      cmd << "-I #{@foundation.installs}/lib/swift"
-      execute cmd.join(" ")
-      execute "file #{mainFile}"
-
-      # Clang
-      cmd = ["cd #{@builds} &&"]
-      cmd << "#{@ndk.toolchain}/bin/armv7a-linux-androideabi#{@ndk.api}-clang -fuse-ld=gold -pie"
-      cmd << "-v"
-      cmd << "-B #{@ndk.toolchain}/bin"
-      cmd << "#{@swift.installs}/lib/swift/android/armv7/swiftrt.o"
-      cmd << mainFile
-      # cmd << "-Xlinker --verbose"
-      cmd << "-lswiftCore"
-      cmd << "-lswiftGlibc"
-      cmd << "-lswiftSwiftOnoneSupport"
-      cmd << "-lswiftDispatch"
-      cmd << "-lBlocksRuntime"
-      cmd << "-lc++_shared"
-      cmd << "-lFoundation"
-      cmd << "-L #{@ndk.toolchain}/lib/gcc/arm-linux-androideabi/4.9.x" # Link the Android NDK's libc++ and libgcc.
-      cmd << "-L #{@foundation.installs}/lib/swift/android"
-      cmd << "-L #{@dispatch.installs}/lib/swift/android"
-      cmd << "-L #{@swift.installs}/lib/swift/android/armv7"
-
-      cmd << "-o #{@binary}"
-      execute cmd.join(" ")
-      execute "file #{@binary}"
-      # execute "readelf -h #{@binary}" # Run run this on Linux
+     execute "cd #{@builds} && #{@swiftc} -emit-executable -o #{@binary} #{@sources}/main.swift"
+     execute "file #{@binary}"
+     copyLibs()
    end
 
 end
