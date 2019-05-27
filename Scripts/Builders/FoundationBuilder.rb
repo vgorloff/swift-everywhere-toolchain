637ce5190576e7ebf15df906ca45e9ed4b47c720
Vlad Gorlov <vlad.gorlov@piavita.com>
2019-05-27 22:32:31 +0200

    Fixes build for other architectures.

--- Scripts/Builders/FoundationBuilder.rb
+++ Scripts/Builders/FoundationBuilder.rb
@@ -108,6 +108,7 @@ class FoundationBuilder < Builder
    def configurePatches(shouldEnable = true)
       configurePatchFile("#{@patches}/CMakeLists.txt.diff", shouldEnable)
       configurePatchFile("#{@patches}/cmake/modules/SwiftSupport.cmake.diff", shouldEnable)
+      configurePatchFile("#{@patches}/Foundation/CGFloat.swift.diff", shouldEnable)
       configurePatchFile("#{@patches}/Foundation/FileManager.swift.diff", shouldEnable)
       configurePatchFile("#{@patches}/Foundation/FileManager+POSIX.swift.diff", shouldEnable)
       configurePatchFile("#{@patches}/Foundation/NSObjCRuntime.swift.diff", shouldEnable)
