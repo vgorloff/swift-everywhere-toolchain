21539d14665ede1adf64f35b2c29571de8f498af
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2019-07-17 09:40:19 +0200

    Added missed icu dependency on Linux.

--- Scripts/Shell/bootstrap.sh
+++ Scripts/Shell/bootstrap.sh
@@ -15,7 +15,7 @@ $SA_CMD
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 # Z3 dependency (`apt-get install libz3-dev`) temporary removed due compile error `llvm::Twine(Z3_get_error_msg(Context, Error)));`
 # See: https://reviews.llvm.org/D54391
-SA_CMD="apt-get -y install mc ruby clang python autoconf libtool uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev systemtap-sdt-dev tzdata rsync"
+SA_CMD="apt-get -y install mc git-lfs ruby clang python autoconf libtool uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev systemtap-sdt-dev tzdata rsync"
 echo "Executing command: \"$SA_CMD\""
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 $SA_CMD
