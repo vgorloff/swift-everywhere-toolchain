7f7be5a7965a8f3ac886c46520608d1be7c010ad
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2019-08-14 20:08:18 +0200

    Bump 6e50c51

--- Scripts/Common/Revision.rb
+++ Scripts/Common/Revision.rb
@@ -42,7 +42,7 @@ class Revision
       # master @ 1.0.28: 131bee791658c1beee0a48868b59a3000030702c
       # master @ 1.0.26: ee6688d264d1012e669eda37c357d55e9de62dba
       # master @ 1.0.23: 7beb3585d5edfc24e945afca513c24205e7a47a5
-      return "b148ea0a416f9ecae1835edbefe1468a4754ca31"
+      return "6e50c519dcaf348c7021cd87e9045d08fd137324"
    end
 
    def self.dispatch
