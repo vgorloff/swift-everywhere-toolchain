d271a6eea50ffb6c03180911862456321807a4e8
Brian Gontowski <brian@gontowski.com>
2019-09-25 10:22:31 +0900

    Configure main bundle to use CFFIXED_USER_HOME for Android

--- Scripts/Builders/FoundationBuilder.rb
+++ Scripts/Builders/FoundationBuilder.rb
@@ -145,6 +145,7 @@ EOM
    def configurePatches(shouldEnable = true)
       configurePatchFile("#{@patches}/CMakeLists.txt.diff", shouldEnable)
       configurePatchFile("#{@patches}/CoreFoundation/Base.subproj/SwiftRuntime/CoreFoundation.h.diff", shouldEnable)
+      configurePatchFile("#{@patches}/CoreFoundation/PlugIn.subproj/CFBundle_Executable.c.diff", shouldEnable)
    end
 
    def libs()
