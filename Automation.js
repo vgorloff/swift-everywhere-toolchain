ce11495b4f44680714b760fcdc5b4e6f25a6e1e2
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2020-10-23 22:23:41 +0200

    Added Cmark builder.

--- Automation.js
+++ Automation.js
@@ -1,6 +1,8 @@
 var Tool = require("./lib/Tool");
 var LLVMBuilder = require("./lib/Builders/LLVMBuilder")
 var SwiftStdLibBuilder = require("./lib/Builders/SwiftStdLibBuilder")
+var SwiftBuilder = require("./lib/Builders/SwiftBuilder")
+var CMarkBuilder = require("./lib/Builders/CMarkBuilder")
 
 module.exports = class Automation extends Tool {
   run() {
@@ -23,6 +25,10 @@ module.exports = class Automation extends Tool {
       new LLVMBuilder().runAction(action)
     } else if (component == "stdlib") {
       this.archs.forEach(item => new SwiftStdLibBuilder(item).runAction(action))
+    } else if (component == "swift") {
+      new SwiftBuilder().runAction(action)
+    } else if (component == "cmark") {
+      new CMarkBuilder().runAction(action)
     } else {
       this.logError(`! Unknown component \"${component}\".`);
       this.usage();
@@ -32,34 +38,34 @@ module.exports = class Automation extends Tool {
   usage() {
     this.print("\nBuilding Toolchain with One Action:\n", 33);
 
-    this.print("   $ make bootstrap\n", 36);
+    this.print("   $ node main.js bootstrap\n", 36);
 
     this.print("Building Toolchain Step-by-Step:\n", 33);
 
     this.print("1. Checkout sources:", 32);
-    this.print("   $ make checkout\n", 36);
+    this.print("   $ node main.js checkout\n", 36);
 
     this.print("2. Build toolchain:", 32);
-    this.print("   $ make build\n", 36);
+    this.print("   $ node main.js build\n", 36);
 
     this.print("3. Install toolchain:", 32);
-    this.print("   $ make install\n", 36);
+    this.print("   $ node main.js install\n", 36);
 
     this.print("4. Archive toolchain:", 32);
-    this.print("   $ make archive\n", 36);
+    this.print("   $ node main.js archive\n", 36);
 
     this.print("5. (Optional) Verify toolchain build:", 32);
-    this.print("   $ make test\n", 36);
+    this.print("   $ node main.js test\n", 36);
 
     this.print("6. (Optional) Clean toolchain build:", 32);
-    this.print("   $ make clean\n", 36);
+    this.print("   $ node main.js clean\n", 36);
 
     this.print("Building certain component (i.e. llvm, icu, xml, ssl, curl, swift, dispatch, foundation):\n", 33);
 
     this.print("To build only certain component:", 32);
-    this.print("   $ make build:llvm\n", 36);
+    this.print("   $ node main.js llvm:build\n", 36);
 
     this.print("To clean only certain component:", 32);
-    this.print("   $ make clean:llvm\n", 36);
+    this.print("   $ node main.js llvm:clean\n", 36);
   }
 };
