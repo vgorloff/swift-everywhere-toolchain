4477a5667e9efebf094b8eda9771cd39201af4f6
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2022-01-06 13:41:20 +0100

    Structure update.

--- Tests/TestAutomation.js
+++ Tests/TestAutomation.js
@@ -1,8 +1,8 @@
 const Tool = require("../lib/Tool");
 const ExecutableBuilder = require("./sample-exe/Builder");
 const LibraryBuilder = require("./sample-lib/Builder");
-const PackageBuilder = require("./sample-package/Builder");
-const ADB = require("./ADB")
+const PackageBuilder = require("./sample-pkg/Builder");
+const ADB = require("./ADB");
 
 module.exports = class TestAutomation extends Tool {
   constructor() {
@@ -17,9 +17,9 @@ module.exports = class TestAutomation extends Tool {
       process.exit(1);
     } else {
       if (action == "verify") {
-        this.verify()
+        this.verify();
       } else if (action == "clean") {
-        this.clean()
+        this.clean();
       } else {
         var components = action.split(":");
         if (components.length == 2) {
@@ -38,7 +38,7 @@ module.exports = class TestAutomation extends Tool {
       this.archs.forEach((item) => new ExecutableBuilder(component, item).runAction(action));
     } else if (component == "lib") {
       this.archs.forEach((item) => new LibraryBuilder(component, item).runAction(action));
-    } else if (component == "package") {
+    } else if (component == "pkg") {
       this.archs.forEach((item) => new PackageBuilder(component, item).runAction(action));
     } else {
       this.logError(`! Unknown component \"${component}\".`);
@@ -48,26 +48,28 @@ module.exports = class TestAutomation extends Tool {
   }
 
   verify() {
-    ADB.verify()
+    ADB.verify();
   }
 
   build() {
-    this.runComponentAction("exe", "build")
-    this.runComponentAction("lib", "build")
-    this.runComponentAction("package", "build")
+    this.runComponentAction("exe", "build");
+    this.runComponentAction("lib", "build");
+    this.runComponentAction("pkg", "build");
   }
 
   clean() {
-    this.runComponentAction("exe", "clean")
-    this.runComponentAction("lib", "clean")
-    this.runComponentAction("package", "clean")
+    this.runComponentAction("exe", "clean");
+    this.runComponentAction("lib", "clean");
+    this.runComponentAction("pkg", "clean");
   }
 
   /** @private */
   usage() {
     this.print("\n1. Build project:", 32);
-    this.print("   $ node main.js project:build\n", 36);
-    this.print("   Where the `project` is one of: exe, lib, package\n", 36);
+    this.print("   $ node main.js <project>:build", 36);
+    this.print("   $ node main.js <project>:build arch:<arch>\n", 36);
+    this.print("   Where the <project> is one of: [exe|lib|pkg]", 36);
+    this.print("   Where the <arch> is one of: [armv7a|aarch64|x86|x86_64]\n", 36);
 
     this.print(
       "2. Enable USB Debugging on Android device. Install Android Tools for macOS. Connect Android device and Verify ADB shell setup.",
@@ -83,21 +85,13 @@ module.exports = class TestAutomation extends Tool {
     this.print(help, 36);
 
     this.print("3. Deploy and run project on Android Device or Simulator.", 32);
-    var help = `
-   $ node main.js project:deploy arch:armv7a
-   $ node main.js project:deploy arch:aarch64
-   $ node main.js project:deploy arch:x86
-   $ node main.js project:deploy arch:x86_64
-`;
-    this.print(help, 36);
+    this.print(`   $ node main.js <project>:deploy arch:<arch>\n`, 36);
 
     this.print("4. (Optional) Clean deployed project:", 32);
-    this.print("   $ node main.js project:undeploy arch:armv7a", 36);
-    this.print("   $ node main.js project:undeploy arch:aarch64", 36);
-    this.print("   $ node main.js project:undeploy arch:x86", 36);
-    this.print("   $ node main.js project:undeploy arch:x86_64", 36);
+    this.print("   $ node main.js <project>:undeploy arch:<arch>", 36);
 
     this.print("\n5. (Optional) Clean project:", 32);
-    this.print("   $ node main.js project:clean\n", 36);
+    this.print("   $ node main.js <project>:clean", 36);
+    this.print("   $ node main.js <project>:clean arch:<arch>\n", 36);
   }
 };
